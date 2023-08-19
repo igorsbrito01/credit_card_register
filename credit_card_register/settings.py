3f1f7251875c8dbb2f52e1f120ea4bd226496ab5
Igor Brito <igorbrito@onsign.tv>
2023-08-18 22:31:18 -0300

    Add postgres connection

--- credit_card_register/settings.py
+++ credit_card_register/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,8 +77,12 @@ WSGI_APPLICATION = "credit_card_register.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB", default="credit_card_register"),
+        "USER": config("DB_USER", default="dev_user"),
+        "PASSWORD": config("DB_PASS", default="dev_pas_9898"),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", default="5432"),
     }
 }
 
